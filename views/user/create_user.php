512219d5534771e2886c185457ffeb52c09610e9
ipallares <ivan@ipallares.com>
2018-07-29 16:34:50 +0200

    Tune create_user for messages (conf and error) and input names and ids

--- views/user/create_user.php
+++ views/user/create_user.php
@@ -4,20 +4,29 @@
 
 <h1>Register a new user</h1>
 
+<?php
+if(isset($confMsg)) {
+    echo "<span class='label label-success def-shop-msg'>$confMsg</span>";
+}
+if(isset($errorMsg)) {
+    echo "<span class='label label-danger def-shop-msg'>$errorMsg</span>";
+}
+?>
+
 <form action="<?php echo $createUserUrl ?>" method="POST">
     <div class="input-group">
         <span class="input-group-addon">Email Address</span>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" id="email" name="email">
     </div>
 
     <div class="input-group">
-        <span class="input-group-addon" id="basic-addon1">User Name</span>
-        <input type="text" class="form-control">
+        <span class="input-group-addon" id="basic-addon1">Name</span>
+        <input type="text" class="form-control" id="name" name="name">
     </div>
 
     <div class="input-group">
         <span class="input-group-addon" id="basic-addon1">Password</span>
-        <input type="password" class="form-control">
+        <input type="password" class="form-control" id="password" name="password">
     </div>
 
 
